4f1843ec00d6d1c8fcf075f262733c0b826ce56d
agapinank <agapinank@gmail.com>
2020-07-13 21:23:17 -0700

    working js file with simple html set up
    Signed-off-by: agapinank <agapinank@gmail.com>

--- script.js
+++ script.js
@@ -1,110 +1,93 @@
-// Questions array with answers: object properties 
+function generateQuiz() {
+  const output = [];
+  questions.forEach((currentQuestion, questionNumber) => {
+    const answers = [];
 
-let questions = [{
- question: "2+2 = n",
- answers: {
-  a: '5',
-  b: '9',
-  c: '10',
-  d: '4',
- },
- correctAnswer: 'd'
-},
-{
- question: "2+3 = n",
- answers: {
-  a: '5',
-  b: '9',
-  c: '10',
-  d: '4',
- },
- correctAnswer: 'a',
-},
-{
- question: "2+4 = n",
- answers: {
-  a: '5',
-  b: '6',
-  c: '10',
-  d: '4',
- },
- correctAnswer: 'b',
-},
-{
- question: "2+12 = n",
- answers: {
-  a: '5',
-  b: '9',
-  c: '14',
-  d: '4',
- },
- correctAnswer: 'c',
-}];
-
-// helper functions : to show quiz, submit answers, and show results
-
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
-
- function showQuestions(questions, quizContainer) {
-   // postResponse is the place to store the answers choices
-  let postResponse = [];
-  let answers;
-
-  for (i = 0; i < questions.length; i++) {
-    //resets the list of answers
-   answers=[];
-   // for each available answer to this question add an html radio button
-   for( letter in questions[i].answers){
- 
-    answers.push(
-      '<label>'
-      + '<input type ="radio" name = "question' + i +'" value="'+letter+'">'
-      + letter +': '
-      + questions[i].answers[letter]
-      + '</label>'
-    );
-  }
-
-//question and it's answers will push to resultsContainer
- postResponse.push(
-  '<div class="question">' + questions[i].question + '</div>'
-  + '<div class ="answers">' + answers.join(' ') + '</div>'
-  );
- }
- quizContainer.innerHTML = postResponse.join(' ');
-}
-
- function showResults(questions, quizContainer, resultsContainer) {
-   //queries DOM for all answer containers from quiz
-  let answerContainers = quizContainer.querySelectorAll('.answers');
-  // track users and answers
-  let playerPick = '';
-  let correctPick =' ';
-
-  for (i=0;i<questions.length; i++){
-
-    playerPick =(answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
-
-    if(playerPick === questions[i].correctPick){
-      //correct answers will increase by 1 & turn green, red for wrong or blank
-      numCorrect++;
-      answerContainers[i].style.color ='lightgreen';
-    }else{
-      answerContainers[i].style.color ='red';
+    for (letter in currentQuestion.answers) {
+      answers.push(
+        `<label>
+          <input type="radio" name="question${questionNumber}" value="${letter}">
+          ${letter} :
+          ${currentQuestion.answers[letter]}
+          </label>`
+      );
     }
-  } //displays # of correct answer out of total #
-  resultsContainer.innerHTML= numCorrect + ' out of ' + questions.length;
- }
+    output.push(
+      `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join('')} </div>`
+    );
+  });
 
- showQuestions(questions, quizContainer);
+  quizBox.innerHTML = output.join('');
+}
+function showResults() {
+  const answerContainers = quizBox.querySelectorAll('.answers');
 
- submitButton.onClick = function () {
-  showResults(questions, quizContainer, resultsContainer)
- }
+  let correctAnswer = 0;
+
+  questions.forEach((currentQuestion, questionNumber) => {
+    const answerContainer = answerContainers[questionNumber];
+    const selector = `input[name=question${questionNumber}]:checked`;
+    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+    if (userAnswer === currentQuestion.correctAnswer) {
+      correctAnswer++;
+
+      answerContainers[questionNumber].style.color = 'lightgreen';
+    } else {
+      answerContainers[questionNumber].style.color = 'red';
+    }
+  });
+  resultBox.innerHTML = `${correctAnswer} out of ${questions.length}`;
 }
 
-let quizContainer = document.getElementById('quiz');
-let resultsContainer = document.getElementById('results');
-let submitButton = document.getElementById('submit');
+let quizBox = document.getElementById('quiz');
+let resultBox = document.getElementById('results');
+let submitBtn = document.getElementById('submitButton');
 
-generateQuiz(questions, quizContainer, resultsContainer, submitButton);
+let questions = [
+  {
+    question: '2+2 = n',
+    answers: {
+      a: '5',
+      b: '9',
+      c: '10',
+      d: '4',
+    },
+    correctAnswer: 'd',
+  },
+  {
+    question: '2+3 = n',
+    answers: {
+      a: '5',
+      b: '9',
+      c: '10',
+      d: '4',
+    },
+    correctAnswer: 'a',
+  },
+  {
+    question: '2+4 = n',
+    answers: {
+      a: '5',
+      b: '6',
+      c: '10',
+      d: '4',
+    },
+    correctAnswer: 'b',
+  },
+  {
+    question: '2+12 = n',
+    answers: {
+      a: '5',
+      b: '9',
+      c: '14',
+      d: '4',
+    },
+    correctAnswer: 'c',
+  },
+];
+
+generateQuiz();
+
+submitButton.addEventListener('click', showResults);
